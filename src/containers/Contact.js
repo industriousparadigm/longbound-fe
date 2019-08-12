e90c04ba2912e6910ca7babf5d27863c8133d040
mrsalvini <dsgmcosta@gmail.com>
2019-08-12 09:46:29 +0100

    test netlify form

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -19,6 +19,21 @@ const Contact = () => {
         <Form.TextArea placeholder='please type your message here...' rows={10} required />
         <Form.Button fluid color='black' size='massive'>Submit</Form.Button>
       </Form>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+
     </div>
   )
 }
