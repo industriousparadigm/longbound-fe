b5445ecd84805764a39cff64e3b79227885e5b2d
mrsalvini <dsgmcosta@gmail.com>
2019-08-29 14:07:19 +0100

    materialize css + form update

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -5,19 +5,15 @@ import { Form, Button } from 'semantic-ui-react'
 const Contact = () => {
   return (
     <div className="main">
-      <Form
+      {/* <Form
         id="contact-form"
         size='big'
         name="contact"
         method='POST'
         data-netlify="true"
-      >
-        <p>
-          You can get in touch using <strong>team[at]longbound[dot]com</strong> or via the form below.
-      </p>
-        <br />
-        <Form.Group widths='equal' name="deets" >
-          {/* hidden input for Netlify */}
+      > */}
+
+      {/* <Form.Group widths='equal' name="deets" >
           <input type="hidden" name="form-name" value="contact" />
 
           <Form.Input
@@ -45,10 +41,55 @@ const Contact = () => {
           className="fluid ui big black button"
         >
           Submit
-        </button>
+        </button> */}
+      <form
+        id="contact-form"
+        size='big'
+        name="contact"
+        method='POST'
+        data-netlify="true"
+      >
+        <p>
+          You can get in touch using <strong>team[at]longbound[dot]com</strong> or via the form below.
+        </p>
         <br />
-      </Form>
-    </div>
+
+        <div class="row">
+          <div class="input-field col s6">
+            <input type='text' name="name" />
+            <label for="name">Name</label>
+          </div>
+          <div class="input-field col s6">
+            <input required type='email' name="email" />
+            <label for="email">Email</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <textarea className="materialize-textarea"
+              rows={10}
+              name="message"
+              required
+            >
+            </textarea>
+            <label for="message">Your message</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+
+            <button
+              type="submit"
+              className="fluid ui big black button"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+
+      </form>
+
+    </div >
   )
 }
 
