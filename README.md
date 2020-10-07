96845ff8f5f62c5dcdf5eb24a438efa992b93579
Diogo Costa <dsgmcosta@gmail.com>
2020-10-07 11:53:18 +0100

    moved env vars to netlify

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-## Longbound website
+[![Netlify Status](https://api.netlify.com/api/v1/badges/39a588ab-4a05-42ac-9ecb-28042d176bf4/deploy-status)](https://app.netlify.com/sites/objective-volhard-4f9034/deploys)
 
-Longbound is a creative R&D and Front-end Innovation team imagining and inventing a better future.
+## The Innovation Brothers website
+
+The Innovation Brothers are a creative R&D and Front-end Innovation team imagining and inventing a better future.
 
 ### Specs
 
 Responsive website built using React + Materialize CSS, React Hooks, React Router, React Socks.
 
-Deployed in September 2019 using Netlify at https://www.longbound.com
+First deployed in September 2019 using Netlify. Visit at https://www.theinnovationbrothers.com
--- src/App.js
+++ src/App.js
@@ -1,77 +1,110 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { BreakpointProvider, Breakpoint } from 'react-socks';
 
-import Header from './containers/Header'
-import Footer from './containers/Footer'
-import Landing from './containers/Landing'
-import About from './containers/About'
-import Contact from './containers/Contact'
-import Services from './containers/Services'
-import SideText from './containers/SideText'
-import NotFound from './containers/NotFound'
-import FormSent from './containers/FormSent'
+import Header from './containers/Header';
+import Footer from './containers/Footer';
+import Landing from './containers/Landing';
+import About from './containers/About';
+import Contact from './containers/Contact';
+import Services from './containers/Services';
+import SideText from './containers/SideText';
+import NotFound from './containers/NotFound';
+import FormSent from './containers/FormSent';
 
-import './App.css'
+import './App.css';
 
+const quotesURL = 'https://longbound-aff6.restdb.io/rest/quotes';
 
-const quotesURL = 'https://longbound-aff6.restdb.io/rest/quotes'
-const apiKey = process.env.REACT_APP_RESTDB_KEY
-
-const getQuotes = () => fetch(quotesURL, {
-  headers: {
-    'Content-Type': 'application/json',
-    'x-apikey': apiKey
-  }
-}).then(res => res.json())
-
+const getQuotes = () =>
+  fetch(quotesURL, {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-apikey': process.env.REACT_APP_RESTDB_KEY,
+    },
+  }).then((res) => res.json());
 
 const App = () => {
-  const [activeSection, setActiveSection] = useState('')
-  const [quotes, setQuotes] = useState(null)
+  const [activeSection, setActiveSection] = useState('');
+  const [quotes, setQuotes] = useState(null);
 
   useEffect(() => {
-    !quotes && getQuotes().then(setQuotes)
-  }, [quotes])
+    !quotes && getQuotes().then(setQuotes);
+  }, [quotes]);
 
-  const handleMenuClick = ({ target }) => setActiveSection(target.text)
+  const handleMenuClick = ({ target }) => setActiveSection(target.text);
 
   const sideText = () => {
     switch (activeSection) {
-      case "About":
-        return "Who we are"
-      case "Services":
-        return "What we do"
-      case "Contact":
-        return "Get in touch"
-      case "404":
-        return "404 error"
-      case "Form sent":
-        return "Thank you"
+      case 'About':
+        return 'Who we are';
+      case 'Services':
+        return 'What we do';
+      case 'Contact':
+        return 'Get in touch';
+      case '404':
+        return '404 error';
+      case 'Form sent':
+        return 'Thank you';
       default:
-        return ""
+        return '';
     }
-  }
+  };
 
   return (
     <Router>
       <BreakpointProvider>
-        <Header handleMenuClick={handleMenuClick} activeSection={activeSection} />
+        <Header
+          handleMenuClick={handleMenuClick}
+          activeSection={activeSection}
+        />
         <Breakpoint medium up>
           <SideText text={sideText()} />
         </Breakpoint>
         <Switch>
-          <Route exact path='/' render={(props) => <Landing {...props} quotes={quotes} />} />
-          <Route exact path='/about' render={(props) => <About {...props} setActiveSection={setActiveSection} />} />
-          <Route exact path='/services' render={(props) => <Services {...props} setActiveSection={setActiveSection} />} />
-          <Route exact path='/contact' render={(props) => <Contact {...props} setActiveSection={setActiveSection} />} />
-          <Route exact path='/contact/success' render={(props) => <FormSent {...props} setActiveSection={setActiveSection} />} />
-          <Route render={(props) => <NotFound {...props} setActiveSection={setActiveSection} />} />
+          <Route
+            exact
+            path="/"
+            render={(props) => <Landing {...props} quotes={quotes} />}
+          />
+          <Route
+            exact
+            path="/about"
+            render={(props) => (
+              <About {...props} setActiveSection={setActiveSection} />
+            )}
+          />
+          <Route
+            exact
+            path="/services"
+            render={(props) => (
+              <Services {...props} setActiveSection={setActiveSection} />
+            )}
+          />
+          <Route
+            exact
+            path="/contact"
+            render={(props) => (
+              <Contact {...props} setActiveSection={setActiveSection} />
+            )}
+          />
+          <Route
+            exact
+            path="/contact/success"
+            render={(props) => (
+              <FormSent {...props} setActiveSection={setActiveSection} />
+            )}
+          />
+          <Route
+            render={(props) => (
+              <NotFound {...props} setActiveSection={setActiveSection} />
+            )}
+          />
         </Switch>
         <Footer />
       </BreakpointProvider>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
